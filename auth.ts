6e69b95f0666aec4a5e8eca0b82e95ef4b569dc9
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2024-02-11 00:07:46 +0000

    changes

--- auth.ts
+++ auth.ts
@@ -15,23 +15,3 @@ export const {
     clientSecret: process.env.AUTH_AUTH0_SECRET,
   })],
 });
-
-//
-// export const config = {
-//   adapter: PrismaAdapter(prisma),
-//   theme: {
-//     logo: "https://next-auth.js.org/img/logo/logo-sm.png",
-//   },
-//   providers: [
-//     Auth0,
-//   ],
-//   callbacks: {
-//     authorized({ request, auth }) {
-//       const { pathname } = request.nextUrl
-//       if (pathname === "/middleware-example") return !!auth
-//       return true
-//     },
-//   },
-// } satisfies NextAuthConfig
-//
-// export const { handlers, auth, signIn, signOut } = NextAuth(config)
